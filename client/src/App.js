85a35ab75c97d1c145114f56ebdbfd9919ed75a8
Federico Ortiz <sib2000@gmail.com>
2021-11-10 19:59:14 -0300

    Me vuelvo locooooo!

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,10 @@ import {
   Route,
 } from "react-router-dom";
 import LoginAdmin from "../src/components/LogInAdmin/Login"
-
-
 import NavBar from "./components/navBar";
 import Home from "./views/home";
 import LogIn from "./views/login";
-import ProductsAdmin from "./components/ProductsAdmin/ProductsAdmin";
+import ProductsAdmin from "./components/productsAdmin/productsAdmin";
 import CategoriesAdmin from "./components/CategoriesAdmin/CategoriesAdmin";
 import ProductForm from "./components/ProductsAdmin/ProductForm";
 import CategoryForm from "./components/CategoriesAdmin/CategoryForm";
