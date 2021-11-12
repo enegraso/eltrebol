c066971c990d16d68e7021c405e56e8609ab50c0
Federico Ortiz <sib2000@gmail.com>
2021-11-12 00:43:46 -0300

    No hice nada

--- client/src/components/OrdersAdmin/OrderAdmin.js
+++ client/src/components/OrdersAdmin/OrderAdmin.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import { useSelector} from "react-redux";
+import { useSelector } from "react-redux";
+import "./orderAdmin.css";
 
 const OrderAdmin = () => {
   const pedidoAdmin = useSelector((state) => state.Order.orderAdmin);
@@ -8,67 +9,70 @@ const OrderAdmin = () => {
 
   return (
     <>
-      {}
-      <h2>Pedido { !pedidoAdmin[0].ordercart ? "Tomando pedido..." : pedidoAdmin[0].ordercart} </h2>
-      <div>
-        {!pedidoAdmin[0].client
-          ? "No hay pedido"
-          : "Cliente: " + pedidoAdmin[0].client}
-      </div>
-      <div>
-        {!pedidoAdmin[0].address
-          ? "Sin dirección"
-          : "Dirección: " + pedidoAdmin[0].address}
-      </div>
-      <div>
-        {!pedidoAdmin[0].cellphone
-          ? "Sin dirección"
-          : "Dirección: " + pedidoAdmin[0].cellphone}
-      </div>
-      <div>
-        <h4>Items</h4>
-      </div>
-      <table border="1">
-        <tr bgcolor="green">
-          <td color="white">Cantidad</td>
-          <td>Producto</td>
-          <td>PU</td>
-          <td>Importe</td>
-          <td>Acción</td>
-        </tr>
-        {pedidoAdmin[0].orderlines.map((line) => {
-          return (
-            <tr bgcolor="lightgreen" key={line.id}>
-              <td>{line.quantity}</td>
-              <td>{line.product.name}</td>
-              <td>{line.product.price}</td>
-              <td>{line.subtotal}</td>
-              <td>
-                <button> MODIFICAR </button>
-              </td>
-            </tr>
-          );
-        })}
-      </table>
-      <div>
-        {!pedidoAdmin[0].subtotal
-          ? 0
-          : "Importe: " + pedidoAdmin[0].subtotal}
-      </div>
-      <div>
-        {pedidoAdmin[0].delivery === true
-          ? "Enviar a domicilio"
-          : "Retira en comercio"}
-      </div>
-      <div>
-        {pedidoAdmin[0].payd === true
-          ? "Pedido pagado"
-          : "Paga al recibir pedido"}
-      </div>{" "}
-      <div>
-        <button> Tomar </button>
-        <button> Rechazar </button>
-        <button> Volver </button>
+      <div className="contenOrder">
+        <div className="titleOrder">
+        <h2>
+          Pedido{" "}
+          {!pedidoAdmin[0].ordercart
+            ? "Tomando pedido..."
+            : pedidoAdmin[0].ordercart}{" "}
+        </h2>
+        </div>
+        <div>
+          {!pedidoAdmin[0].client
+            ? "No hay pedido"
+            : "Cliente: " + pedidoAdmin[0].client}
+        </div>
+        <div>
+          {!pedidoAdmin[0].address
+            ? "Sin dirección"
+            : "Dirección: " + pedidoAdmin[0].address}
+        </div>
+        <div>
+          {!pedidoAdmin[0].cellphone
+            ? "Sin dirección"
+            : "Dirección: " + pedidoAdmin[0].cellphone}
+        </div>
+        <table border="1">
+          <tr>
+            <td className="titleLines">Cantidad</td>
+            <td className="titleLines">Producto</td>
+            <td className="titleLines">PU</td>
+            <td className="titleLines">Importe</td>
+            <td className="titleLines">Acción</td>
+          </tr>
+          {pedidoAdmin[0].orderlines.map((line) => {
+            return (
+              <tr bgcolor="lightgreen" key={line.id}>
+                <td>{line.quantity}</td>
+                <td>{line.product.name}</td>
+                <td>{line.product.price}</td>
+                <td>{line.subtotal}</td>
+                <td>
+                  <button> MODIFICAR </button>
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+        <div>
+          {!pedidoAdmin[0].subtotal ? 0 : "Importe: " + pedidoAdmin[0].subtotal}
+        </div>
+        <div>
+          {pedidoAdmin[0].delivery === true
+            ? "Enviar a domicilio"
+            : "Retira en comercio"}
+        </div>
+        <div>
+          {pedidoAdmin[0].payd === true
+            ? "Pedido pagado"
+            : "Paga al recibir pedido"}
+        </div>{" "}
+        <div>
+          <button> Procesar </button>
+          <button> Rechazar </button>
+          <button> Volver </button>
+        </div>
       </div>
     </>
   );
