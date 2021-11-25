fc1e43958a325934da27b0912f0e6683debf7ae6
Federico Ortiz <sib2000@gmail.com>
2021-11-25 09:45:49 -0300

    Probando detalles de home como solicitó el cliente

--- client/src/views/home/index.js
+++ client/src/views/home/index.js
@@ -4,10 +4,13 @@ import SearchBar from '../../components/searchBar';
 import ProductGrid from '../../components/productGrid';
 import {getAllProducts} from '../../store/actions/products';
 import Cart from '../carrito';
+import Paso1 from '../../components/checkout/paso1'
+import Spinner from '../../components/spinner';
 
 export default function Home(){
 
     const dispatch = useDispatch();
+ 
 
     useEffect(()=>{
         console.log('entrando al home')
@@ -16,13 +19,14 @@ export default function Home(){
 
     const [loading] = useState(false);
     const productos = useSelector(state => state.Product.allProducts);
-    console.log(productos)
+    const verCarro = useSelector(state => state.Carrito.guestCartProd)
+    console.log(productos," - ", verCarro)
 
     return(
         <div className='container'>
         <SearchBar/>
-        { productos.length ? <ProductGrid loading={loading} items={productos}/> : <p>No se encontraron productos</p>}
-        <Cart />
+        { productos.length ? <><ProductGrid loading={loading} items={productos}/>{ verCarro.length ? <><Cart /> <Paso1 /></> : ""}</> : <p><Spinner /> </p>}
+        
         </div>
     )
 }
