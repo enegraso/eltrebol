980699ba1cab7385cbfcc53bc016a9324f3437f8
Federico Ortiz <sib2000@gmail.com>
2021-12-18 10:16:28 -0300

    Eliminando webpush del back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,7 +4,6 @@ const categoriesRoutes = require('./categories')
 const productsRoutes = require('./products')
 const ordersRoutes = require('./orders')
 const mpagoRoutes = require('./mercadopago')
-const pushRoutes = require('./webpush')
 
 const router = Router();
 
@@ -13,7 +12,6 @@ router.use('/categories', categoriesRoutes)
 router.use('/products', productsRoutes)
 router.use('/orders', ordersRoutes)
 router.use('/mp', mpagoRoutes)
-router.use('/push', pushRoutes)
 
 router.get("/", (req,res) => {
     res.status(200).json({message:"backend - Chacinados El Trebol"})
