fdd4f4394fe8cf95b3ac3bf03ca0290ad455bb28
Federico Ortiz <sib2000@gmail.com>
2022-01-28 18:17:38 -0300

    Acomodando la imagen en vista escritorio

--- client/src/components/OrdersAdmin/OrderAdmin.js
+++ client/src/components/OrdersAdmin/OrderAdmin.js
@@ -5,21 +5,19 @@ import { Link } from "react-router-dom";
 import { prepOrder } from "../../store/actions/orders";
 import swal from "sweetalert2";
 
-
 //Material UI-------------------------->
 
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper'
-import Grid from '@mui/material/Grid';
-import {ButtonOne} from '../reutilizables/Button'
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { ButtonOne } from "../reutilizables/Button";
 
 //-------------------------------------->
-
 
 const OrderAdmin = () => {
   const dispatch = useDispatch();
@@ -111,32 +109,29 @@ const OrderAdmin = () => {
 
   return (
     <Grid
-    spacing={2}
-    direction="column"
-    alignItems="center"
-/*     style={{
+      spacing={2}
+      direction="column"
+      alignItems="center"
+      /*     style={{
       'marginTop':'100px',
       'marginBottom':'100px'
     }} */
     >
       <Grid item bgcolor="lightgreen">
-          <h5>
-            Pedido{" "}
-            {!pedidoAdmin[0].id ? "Tomando pedido..." : pedidoAdmin[0].id}
-            {" - "}
-            {pedidoAdmin[0].status === "pending"
-              ? "Pendiente"
-              : "Completo y entregado"}
-            <br />
-            {pedidoAdmin[0].status === "pending"
-              ? "Ingreso " + d.toLocaleString()
-              : "Entregado " + d.toLocaleString()}
-          </h5>
-     
-        </Grid>
-        <Grid item>
-
-          <div>
+        <h5>
+          Pedido {!pedidoAdmin[0].id ? "Tomando pedido..." : pedidoAdmin[0].id}
+          {" - "}
+          {pedidoAdmin[0].status === "pending"
+            ? "Pendiente"
+            : "Completo y entregado"}
+          <br />
+          {pedidoAdmin[0].status === "pending"
+            ? "Ingreso " + d.toLocaleString()
+            : "Entregado " + d.toLocaleString()}
+        </h5>
+      </Grid>
+      <Grid item>
+        <div>
           {!pedidoAdmin[0].client
             ? "No hay pedido"
             : "Cliente: " + pedidoAdmin[0].client}
@@ -151,76 +146,86 @@ const OrderAdmin = () => {
             ? "Sin teléfono"
             : "Teléfono: " + pedidoAdmin[0].cellphone}
         </div>
-        
-        </Grid>
-        <Grid item>
-
-        <TableContainer component={Paper}     style={{
-      'marginTop':'20px',
-      'marginBottom':'20px'
-    }}>
-        <Table size="small" aria-label="a dense table">
-          <TableHead bgcolor="lightgreen">
-            <TableRow>
-            <TableCell padding="none" align="center" width={1}>
-              Cantidad
-            </TableCell>
-            <div className="imageProduct"><TableCell></TableCell></div>
-            <TableCell padding="checkbox" align="center">
-              Producto
-            </TableCell>
-            <TableCell padding="checkbox" align="center" width={1}>
-              PU
-            </TableCell>
-            <TableCell padding="checkbox" align="center" width={1}>
-              Importe
-            </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-
-          {pedidoAdmin[0].orderlines.map((line) => {
-            return (
-              <TableRow 
-              key={line.id}
-              >
-                <TableCell scope="row"  padding="none" align="center" width={1}>
-                  {!Number.isInteger(line.quantity)
-                    ? line.quantity.toFixed(3).replace(".", ",")
-                    : line.quantity}{" "}
-                  {/* - {!line.product ? "envio" : line.product.units}{" "} */}
+      </Grid>
+      <Grid item>
+        <TableContainer
+          component={Paper}
+          style={{
+            marginTop: "20px",
+            marginBottom: "20px",
+          }}
+        >
+          <Table size="small" aria-label="a dense table">
+            <TableHead bgcolor="lightgreen">
+              <TableRow>
+                <TableCell padding="none" align="center" width={1}>
+                  Cantidad
                 </TableCell>
-                <div  className="imageProduct"><TableCell padding="none" align="right" width={1}>
-                  <img
-                    className="imageProduct"
-                    src={!line.product ? imageDelivery : line.product.image}
-                    />
-                </TableCell></div>
-                <TableCell padding="none" /* align="right" */>
-                  {!line.product ? "Envio a domicilio" : line.product.name}
+                {/* <div className="imageProduct"> */}
+                  <TableCell padding="none" align="center" width={1}></TableCell>
+                {/* </div> */}
+                <TableCell padding="checkbox" align="center" width={3}>
+                  Producto
                 </TableCell>
-                <TableCell padding="none" align="right" width={1}>
-                  {Number(!line.product ? line.price : line.product.price)
-                    .toFixed(2)
-                    .replace(".", ",")}
+                <TableCell padding="checkbox" align="center" width={1}>
+                  PU
                 </TableCell>
-                <TableCell padding="none" align="right" width={1}><b>{Number(line.subtotal).toFixed(2).replace(".", ",")}</b></TableCell>
+                <TableCell padding="checkbox" align="center" width={1}>
+                  Importe
+                </TableCell>
               </TableRow>
-            );
-          })}
-
-          </TableBody>
-        </Table>
+            </TableHead>
+            <TableBody>
+              {pedidoAdmin[0].orderlines.map((line) => {
+                return (
+                  <TableRow key={line.id}>
+                    <TableCell
+                      scope="row"
+                      padding="none"
+                      align="center"
+                      width={1}
+                    >
+                      {!Number.isInteger(line.quantity)
+                        ? line.quantity.toFixed(3).replace(".", ",")
+                        : line.quantity}{" "}
+                      {/* - {!line.product ? "envio" : line.product.units}{" "} */}
+                    </TableCell>
+                    {/* <div className="imageProduct"> */}
+                      <TableCell padding="none" /* align="right" */  width={1}>
+                         <img
+                          className="imageProduct"
+                          src={
+                            !line.product ? imageDelivery : line.product.image
+                          }
+                        /> 
+                      </TableCell>
+                    {/* </div> */}
+                    <TableCell padding="none" /* align="right" */ width={5}>
+                      {!line.product ? "Envio a domicilio" : line.product.name}
+                    </TableCell>
+                    <TableCell padding="none" align="right" width={1}>
+                      {Number(!line.product ? line.price : line.product.price)
+                        .toFixed(2)
+                        .replace(".", ",")}
+                    </TableCell>
+                    <TableCell padding="none" align="right" width={1}>
+                      <b>
+                        {Number(line.subtotal).toFixed(2).replace(".", ",")}
+                      </b>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
         </TableContainer>
-        
-        </Grid>
-        <Grid item>
-          
+      </Grid>
+      <Grid item>
         <div className="importePedido">
           {!pedidoAdmin[0].subtotal
             ? 0
             : "Importe AR$: " +
-            pedidoAdmin[0].subtotal.toFixed(2).replace(".", ",")}
+              pedidoAdmin[0].subtotal.toFixed(2).replace(".", ",")}
         </div>
         {pedidoAdmin[0].status === "pending" ? (
           <>
@@ -237,40 +242,39 @@ const OrderAdmin = () => {
           </>
         ) : (
           <> </>
-          )}
-
-        </Grid>
-        <Grid item>
-          {pedidoAdmin[0].status === "pending" ? (
-            <button className="btn btn-primary" onClick={handleClick}>
-              {" "}
-              Preparar{" "}
-            </button>
-          ) : (
-            ""
-          )}
-          <button className="btn btn-secondary" onClick={() => window.print()}>
+        )}
+      </Grid>
+      <Grid item>
+        {pedidoAdmin[0].status === "pending" ? (
+          <button className="btn btn-primary" onClick={handleClick}>
             {" "}
-            Imprimir{" "}
+            Preparar{" "}
           </button>
-          {pedidoAdmin[0].status === "pending" ? (
-            <button className="btn btn-info" onClick={handleClickRej}>
-              {" "}
-              Rechazar{" "}
-            </button>
-          ) : (
-            ""
-          )}
-          <button
-            className="btn btn-link"
-            onClick={() => {
-              window.history.go(-1);
-            }}
-          >
+        ) : (
+          ""
+        )}
+        <button className="btn btn-secondary" onClick={() => window.print()}>
+          {" "}
+          Imprimir{" "}
+        </button>
+        {pedidoAdmin[0].status === "pending" ? (
+          <button className="btn btn-info" onClick={handleClickRej}>
             {" "}
-            Volver{" "}
+            Rechazar{" "}
           </button>
-        </Grid>
+        ) : (
+          ""
+        )}
+        <button
+          className="btn btn-link"
+          onClick={() => {
+            window.history.go(-1);
+          }}
+        >
+          {" "}
+          Volver{" "}
+        </button>
+      </Grid>
     </Grid>
   );
 };
